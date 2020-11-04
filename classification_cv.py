c22d4688912e72147980f63d59772cb9b2f4f9f0
Michael Larionov <6360814+mlarionov@users.noreply.github.com>
2020-11-04 06:41:21 -0600

    Updated the experiments on synthetic data to have more categorical variables

--- classification_cv.py
+++ classification_cv.py
@@ -2,7 +2,7 @@ from classification import *
 import json
 
 if __name__ == '__main__':
-    predictors, y_h, columns = create_data(2)
+    predictors, y_h, columns = create_data(9)
     loo_search = cv_leave_one_out_encoder(predictors, y_h, columns)
     with open("studies/loo_classification_cv.json", 'w') as json_file:
         json.dump(loo_search, json_file, indent=4)
--- hastie_cv.py
+++ hastie_cv.py
@@ -1,7 +1,7 @@
 from classification import *
 import json
 if __name__ == '__main__':
-    predictors, y_h, cols = create_data_hastie(2)
+    predictors, y_h, cols = create_data_hastie(9)
     loo_search = cv_leave_one_out_encoder(predictors, y_h, cols)
     with open("studies/loo_hastie_cv.json", 'w') as json_file:
         json.dump(loo_search, json_file, indent=4)
